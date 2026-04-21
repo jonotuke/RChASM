86613a15a4329e59bc07a776ceab710d29c49be8
Jonathan Tuke <jonotuke@gmail.com>
2026-04-22 06:42:33 +0930

    Clean up

--- R/example_data.R
+++
@@ -1,7 +0,0 @@
-#' example_data
-#'
-#' An example data set for RChASM
-#'
-#' @format A data frame with 266 rows and 26 variables:
-#' @source "Ben Rohrlach"
-"example_data"
--- data-raw/example_data.R
+++
@@ -1,4 +0,0 @@
-## code to prepare `example_data` dataset goes here
-example_file <- fs::path_package("RChASM", "inst/extdata/example_data.tsv")
-example_data <- readr::read_tsv(example_file)
-usethis::use_data(example_data, overwrite = TRUE)
